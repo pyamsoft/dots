a4a6ff8fae0ed8fb8e0ebd4a0eb4205807baac79
pyamsoft <developer.pyamsoft@gmail.com>
2025-11-20 14:08:07 -0800

    Monitor directories

--- dots/scripts/linux/pyamsoft-settings/hosts/nerd2/etc/lsyncd/lsyncd.954.conf.lua
+++ dots/scripts/linux/pyamsoft-settings/hosts/nerd2/etc/lsyncd/lsyncd.954.conf.lua
@@ -1,24 +1,24 @@
 -- https://lsyncd.github.io/lsyncd/manual/config/file/
 settings({
-  logfile = "/tmp/lsyncd.954.log",
-  statusFile = "/tmp/lsyncd.954.status",
-  pidfile = "/tmp/lsyncd.954.pid",
-  nodaemon = true,
+	logfile = "/tmp/lsyncd.954.log",
+	statusFile = "/tmp/lsyncd.954.status",
+	pidfile = "/tmp/lsyncd.954.pid",
+	nodaemon = true,
 })
 
 sync({
-  default.rsync,
+	default.rsync,
 
-  source = "/mnt/MassMedia/2/media",
-  target = "/mnt/MassMedia/1/media",
+	source = "/mnt/MassMedia/2/media/",
+	target = "/mnt/MassMedia/1/media/",
 
-  rsync = {
-    archive = true,
-    compress = false,
-    verbose = true,
-    _extra = {
-      -- delete removed files from target
-      "--delete",
-    },
-  },
+	rsync = {
+		archive = true,
+		compress = false,
+		verbose = true,
+		_extra = {
+			-- delete removed files from target
+			"--delete",
+		},
+	},
 })
--- dots/scripts/linux/pyamsoft-settings/hosts/nerd2/etc/lsyncd/lsyncd.955.conf.lua
+++ dots/scripts/linux/pyamsoft-settings/hosts/nerd2/etc/lsyncd/lsyncd.955.conf.lua
@@ -1,24 +1,24 @@
 -- https://lsyncd.github.io/lsyncd/manual/config/file/
 settings({
-  logfile = "/tmp/lsyncd.955.log",
-  statusFile = "/tmp/lsyncd.955.status",
-  pidfile = "/tmp/lsyncd.955.pid",
-  nodaemon = true,
+	logfile = "/tmp/lsyncd.955.log",
+	statusFile = "/tmp/lsyncd.955.status",
+	pidfile = "/tmp/lsyncd.955.pid",
+	nodaemon = true,
 })
 
 sync({
-  default.rsync,
+	default.rsync,
 
-  source = "/mnt/MassMedia/2/shares",
-  target = "/mnt/MassMedia/1/shares",
+	source = "/mnt/MassMedia/2/shares/",
+	target = "/mnt/MassMedia/1/shares/",
 
-  rsync = {
-    archive = true,
-    compress = false,
-    verbose = true,
-    _extra = {
-      -- delete removed files from target
-      "--delete",
-    },
-  },
+	rsync = {
+		archive = true,
+		compress = false,
+		verbose = true,
+		_extra = {
+			-- delete removed files from target
+			"--delete",
+		},
+	},
 })
