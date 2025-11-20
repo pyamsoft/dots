eb6573c8a7dbd5cc089812bf5d8a7779fb2751cc
pyamsoft <developer.pyamsoft@gmail.com>
2025-11-20 13:45:13 -0800

    Add lsyncd configs

---
+++ dots/scripts/linux/pyamsoft-settings/hosts/nerd2/etc/lsyncd/lsyncd.954.conf.lua
@@ -0,0 +1,24 @@
+-- https://lsyncd.github.io/lsyncd/manual/config/file/
+settings({
+  logfile = "/tmp/lsyncd.954.log",
+  statusFile = "/tmp/lsyncd.954.status",
+  pidfile = "/tmp/lsyncd.954.pid",
+  nodaemon = true,
+})
+
+sync({
+  default.rsync,
+
+  source = "/mnt/MassMedia/2/media",
+  target = "/mnt/MassMedia/1/media",
+
+  rsync = {
+    archive = true,
+    compress = false,
+    verbose = true,
+    _extra = {
+      -- delete removed files from target
+      "--delete",
+    },
+  },
+})
